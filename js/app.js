38361c6b876454ca22a787eb11e110e5f9250149
superrabbitrun007` <244398764@qq.com>
2018-09-29 12:14:48 +0800

    修改图标大小

--- js/app.js
+++ js/app.js
@@ -2,12 +2,28 @@ var map;
 
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 40.7413549, lng: -73.9980244},
+		center: {lat: 23.3790333, lng: 113.76328280000007},
     	zoom: 13
 	});
 	var markers = [];
-	// var marker;
-	// marker = new google.maps.Marker({
+	var locations=[
+		{title:'深圳红树湾', location:{lat: 22.53056, lng: 113.96632799999998}},
+		{title:'欢乐谷', location: {lat: 22.533468, lng: 113.99344819999999}},
+		{title:'深圳白石洲华侨城', location: {lat: 22.533583, lng: 113.96849799999995}},
+		{title:'深圳市盐田区大梅沙东部华侨城', location:{lat:22.631358 ,lng:114.28218500000003 }}];
+
+	// for(i=0) {}
+	locations.map(loc=>{
+		let title=loc.title;
+		let location=loc.location;
+		let marker = new google.maps.Marker({
+			position: location,
+			map: map,
+			title: title
+		});
+		return marker;
+	});
+	// var marker = new google.maps.Marker({
 	// 	position: ,
 	// 	map: map,
 	// 	title: 
@@ -21,6 +37,5 @@ console.log(menu);
 menu.addEventListener('click', function(e) {
 	console.log(e.target);
 	nav.classList.toggle('open');
-	// maping.classList.toggle('move');
 
 });
